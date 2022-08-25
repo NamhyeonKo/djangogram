6a7b3cd4128564b3f5fa045c54a638518869e7dd
haha <hihi@gmail.com>
2022-08-25 18:16:01 +0900

    user 모델 + post 앱 + post 모델 생성

---
+++ djangogram/posts/models.py
@@ -0,0 +1,37 @@
+from tkinter import N
+from django.db import models
+from djangogram.users import models as user_model
+
+#! django에서 게시물이랑 댓글의 생성 수정 시간을 각각 저장하는게 아니라 따로 모델 생성해서 함 ㄷㄷ
+class TimeStampedModel(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Post(TimeStampedModel):
+    author = models.ForeignKey(
+                user_model.User,
+                null=True,
+                on_delete=models.CASCADE,
+                related_name="post_author"
+            )
+    image = models.ImageField(blank=True)
+    caption = models.TextField(blank=True)
+    image_likes = models.ManyToManyField(user_model.User,related_name="post_image_likes")
+
+class Comment(TimeStampedModel):
+    author = models.ForeignKey(
+                user_model.User,
+                null=True,
+                on_delete=models.CASCADE,
+                related_name='post_author'
+            )
+    posts = models.ForeignKey(
+                Post,
+                null=True,
+                on_delete=models.CASCADE,
+                related_name='comment_post'
+            )
+    contents = models.TextField(blank=True)
--- djangogram/users/models.py
+++ djangogram/users/models.py
@@ -1,26 +1,27 @@
+from platform import python_version_tuple
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    """
-    Default custom user model for djangogram.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
-    """
+    #얘 처럼 저장 가능할줄 몰랐음 ㄷㄷ
+    GENDER_CHOICES = [
+        ('M','Male'),
+        ('F','Female'),
+    ]
 
-    #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    user_name = models.CharField(blank=True, max_length=255)
+    profile_photo = models.ImageField(blank=True)   #? imagefield는 뭘 까?
+    website = models.URLField(blank=True)   #? urlfield로 저장하면 일반 charfield와 뭐가 다를까?
+    bio = models.TextField(blank=True)
+    email = models.CharField(blank=True, max_length=255)
+    phone_number = models.CharField(blank=True, max_length=255)
+    gender = models.CharField(blank=True, choices=GENDER_CHOICES, max_length=255)
+    follower = models.ManyToManyField("self")   #! manytomanyfield 이용하면 다대다 db 이용 가능
+    following = models.ManyToManyField("self")
 
     def get_absolute_url(self):
-        """Get url for user's detail view.
-
-        Returns:
-            str: URL for user detail.
-
-        """
         return reverse("users:detail", kwargs={"username": self.username})
